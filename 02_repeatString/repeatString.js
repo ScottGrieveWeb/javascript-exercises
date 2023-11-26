ec82ef605d122ee96b26132aa2aef461d684bf04
ScottGrieveWeb <scottgrieveweb@gmail.com>
2023-11-26 20:40:15 +0000

    Completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = string;
 
+    if (num === 0){
+            return newString = '';
+        } else if (num < 0){
+            return newString = 'ERROR';
+        } else {
+            for (let i = 1; i < num; i++){
+                newString = newString + string;
+            }
+        }
+        
+    return newString;
 };
 
 // Do not edit below this line
