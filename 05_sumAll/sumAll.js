22f96b4f3337b7612092f0611f3b0211a1ee64c8
ScottGrieveWeb <scottgrieveweb@gmail.com>
2023-11-27 18:50:10 +0000

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    let temp;
+
+    if (num1 > num2){
+        
+        temp = num1;
+
+        num1 = num2;
+        num2 = temp;
+
+    }
+
+    if (num1 < 0 || num2 < 0){
+        return 'ERROR';
+    } else if (typeof num1 !== "number" || typeof num2 !== "number"){
+        return 'ERROR';
+    }
+
+    let finalSum = 0;
+
+    for (let i = num1; i <= num2; i++){
+        finalSum += i;
+    }
+
+    return finalSum;
 
 };
 
