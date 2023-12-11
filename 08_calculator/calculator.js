1a36c82d826ecb7efeff6072a5b3c1d979592c68
ScottGrieveWeb <scottgrieveweb@gmail.com>
2023-12-11 16:51:01 +0000

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sumAll = arr.reduce((total, currentValue) => {
+    return total + currentValue;
+  }, 0);
+
+  return sumAll;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let myValue = arr.reduce((a,b)=> a*b);
+  return myValue;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0){
+    return 1;
+  }else {
+    let arr = [];
+
+    for (let i = 1; i <= a; i++){
+      arr.push(i);
+   };
+
+    let myValue = arr.reduce((a,b)=> a*b);
+    return myValue;
+    };
 };
 
 // Do not edit below this line
